72d3abf2218d559f7d4eb0826282ce721ef74850
Jeff Kowalski <jeff.kowalski@gmail.com>
2017-02-06 14:32:41 -0800

    Download permit; encase in thor; use gemfile for bundler

--- permitter.rb
+++ permitter.rb
@@ -1,66 +1,137 @@
+#!/usr/bin/env ruby
+
 require 'mechanize'
 require 'date'
+require 'thor'
 
-@log = Logger.new(STDOUT)
-@log.level = Logger::INFO
 
-@agent = Mechanize.new { |a|
-  a.user_agent_alias = 'Windows IE 10'
-  a.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36'
-#  a.log = @log
-}
+LOGFILE = File.join(Dir.home, '.permitter.log')
+CREDENTIALS_PATH = "credentials.yml"
 
-credentials = YAML.load_file "credentials.yml"
 
-date = YAML.load_file "date.yml"
+class Permitter < Thor
+  no_commands {
+    def redirect_output
+      unless LOGFILE == 'STDOUT'
+        logfile = File.expand_path(LOGFILE)
+        FileUtils.mkdir_p(File.dirname(logfile), :mode => 0755)
+        FileUtils.touch logfile
+        File.chmod 0644, logfile
+        $stdout.reopen logfile, 'a'
+      end
+      $stderr.reopen $stdout
+      $stdout.sync = $stderr.sync = true
+    end
 
-@log.info "(1/6) logging in"
-page = @agent.get "https://select-a-spot.com/bart/"
-page = page.form_with(:id => "site-login") do |login|
-  login.username = credentials[:username]
-  login.password = credentials[:password]
-end.submit
+    def setup_logger
+      redirect_output if options[:log]
 
-@log.info "(2/6) selecting daily reservation"
-page = page.link_with(:href => "/bart/reservations/facilities/?type=daily").click
+      $logger = Logger.new STDOUT
+      $logger.level = options[:verbose] ? Logger::DEBUG : Logger::INFO
+      $logger.info 'starting'
+    end
 
-@log.info "(3/6) selecting lafayette station"
-page = page.form_with(:action => "/bart/reservations/date/") do |form|
-  form.radiobutton_with(:id => "type_id_40").check
-end.submit
+    def get_reservation agent, date
+      reservation = {}
+      page = agent.get "https://www.select-a-spot.com/bart/users/reservations/"
+      doc = page.parser
+      doc.xpath("//table").each { |table|
+        table.xpath("tr").each { |tr|
+          td = tr.xpath("td")
+          reservation[td[0].text] = td[1].text
+        }
+        break if date.to_s == Date.parse(reservation["From:"]).to_s
+      }
+      if date.to_s == Date.parse(reservation["From:"]).to_s
+        return reservation
+      else
+        $logger.error "couldn't find permit"
+        return nil
+      end
+    end
 
-@log.info "(4/6) attempting to reserve #{date}"
-# http://mechanize.rubyforge.org/Mechanize/Page.html
-page = page.form_with(:action => "/bart/reservations/date/") do |form|
-  form.start_date =
-    form.end_date = date.to_s
-end.submit
 
-if (page.body =~ /Reservations cannot be made more than two months in advance/)
-  @log.error "too far in advance"
-else
-  File.open('date.yml', 'w') {|f| f.write date.succ.to_yaml } #Store
+    def download_permit agent, permit_id
+      page = agent.get "/bart/reservations/print_permit/?id=#{permit_id}&date=0"
+      result = JSON.parse page.body
+      if result["success"]
+        page = agent.get "/bart/reservations/permit_pdf/?id=#{permit_id}"
+        filename = page.header["content-disposition"][/filename=(.*)/, 1]
+        File.open(File.join(ENV['HOME'], 'Dropbox/workspace/@print', filename), "wb") do |file|
+          file.write page.body
+        end
+      end
+    end
+  }
 
-  if (page.body =~ /This facility has no availability for the date range you selected. Please choose from one of the following facilities which do have availability,/)
-    @log.info "lafayette full, selecting orinda station"
-    page = page.form_with(:action => "/bart/reservations/date/") do |form|
-      form.radiobutton_with(:id => "type_id_37").check
+  class_option :log,     :type => :boolean, :default => true, :desc => "log output to ~/.permitter.log"
+  class_option :verbose, :type => :boolean, :aliases => "-v", :desc => "increase verbosity"
+
+  desc "get", "get next available permit"
+  def get
+    setup_logger
+
+    @agent = Mechanize.new { |a|
+      a.user_agent_alias = 'Windows IE 10'
+      a.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36'
+      a.log = $logger if options[:verbose]
+    }
+
+    date = YAML.load_file "date.yml"
+    while date.saturday? || date.sunday?
+      date = date.succ
+    end
+
+    $logger.info "(1/6) logging in"
+    page = @agent.get "https://select-a-spot.com/bart/"
+    credentials = YAML.load_file CREDENTIALS_PATH
+    page = page.form_with(:id => "site-login") do |login|
+      login.username = credentials[:username]
+      login.password = credentials[:password]
     end.submit
+
+    $logger.info "(2/6) selecting daily reservation"
+    page = page.link_with(:href => "/bart/reservations/facilities/?type=daily").click
+
+    $logger.info "(3/6) selecting lafayette station"
+    page = page.form_with(:action => "/bart/reservations/date/") do |form|
+      form.radiobutton_with(:id => "type_id_40").check
+    end.submit
+
+    $logger.info "(4/6) attempting to reserve #{date}"
+    # http://mechanize.rubyforge.org/Mechanize/Page.html
+    page = page.form_with(:action => "/bart/reservations/date/") do |form|
+      form.start_date =
+        form.end_date = date.to_s
+    end.submit
+
+    if (page.body =~ /Reservations cannot be made more than two months in advance/)
+      $logger.error "too far in advance"
+    else
+      File.open('date.yml', 'w') {|f| f.write date.succ.to_yaml } #Store
+
+      if (page.body =~ /This facility has no availability for the date range you selected. Please choose from one of the following facilities which do have availability,/)
+        $logger.info "lafayette full, selecting orinda station"
+        page = page.form_with(:action => "/bart/reservations/date/") do |form|
+          form.radiobutton_with(:id => "type_id_37").check
+        end.submit
+      end
+
+      $logger.info "(5/6) reserving"
+      page = page.form_with(:action => "/bart/reservations/reserve/").submit
+
+      $logger.info "(6/6) agreeing to terms and conditions"
+      page = page.link_with(:href => /confirm/).click
+      page = page.form_with(:id => "complete_form") do |form|
+        form.checkbox_with(:name => "conditions").check
+      end.submit
+
+      $logger.info "getting permit"
+      reservation = get_reservation @agent, date
+      download_permit @agent, reservation['Permit #:']
+    end
   end
 
-  @log.info "(5/6) reserving"
-  page = page.form_with(:action => "/bart/reservations/reserve/").submit
-
-  @log.info "(6/6) agreeing to terms and conditions"
-  page = page.link_with(:href => /confirm/).click
-  page = page.form_with(:id => "complete_form") do |form|
-    form.checkbox_with(:name => "conditions").check
-  end.submit
-
-  # page = "/bart/reservations/print_permit/?id=#{id}&date=0"
-  # receive {successs:true}
-  # page = "/bart/reservations/permit_pdf/?id=#{id}"
-  # receive PDF
-
-  p page.body
 end
+
+Permitter.start
