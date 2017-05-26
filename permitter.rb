c51b9295b45eefeac79c44ee6d638f4859346609
Jeff Kowalski <jeff.kowalski@gmail.com>
2017-05-25 20:26:06 -0700

    Downgrade benign errors to warnings; catch and log exceptions

--- permitter.rb
+++ permitter.rb
@@ -72,71 +72,76 @@ class Permitter < Thor
   def get
     setup_logger
 
-    @agent = Mechanize.new { |a|
-      a.user_agent_alias = 'Windows IE 10'
-      a.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36'
-      a.log = $logger if options[:verbose]
-    }
+    begin
+      @agent = Mechanize.new { |a|
+        a.user_agent_alias = 'Windows IE 10'
+        a.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36'
+        a.log = $logger if options[:verbose]
+      }
 
-    date = YAML.load_file "date.yml"
-    while date.saturday? || date.sunday?
-      date = date.succ
-    end
-
-    $logger.info "(1/6) logging in"
-    page = @agent.get "https://select-a-spot.com/bart/"
-    credentials = YAML.load_file CREDENTIALS_PATH
-    page = page.form_with(:id => "site-login") do |login|
-      login.username = credentials[:username]
-      login.password = credentials[:password]
-    end.submit
-
-    $logger.info "(2/6) selecting daily reservation"
-    page = page.link_with(:href => "/bart/reservations/facilities/?type=daily").click
-
-    $logger.info "(3/6) selecting lafayette station"
-    page = page.form_with(:action => "/bart/reservations/date/") do |form|
-      form.radiobutton_with(:id => "type_id_40").check
-    end.submit
-
-    $logger.info "(4/6) attempting to reserve #{date}"
-    # http://mechanize.rubyforge.org/Mechanize/Page.html
-    page = page.form_with(:action => "/bart/reservations/date/") do |form|
-      form.start_date =
-        form.end_date = date.to_s
-    end.submit
-
-    if (page.body =~ /Reservations cannot be made more than two months in advance/)
-      $logger.error "too far in advance"
-      return
-    else
-      File.open('date.yml', 'w') {|f| f.write date.succ.to_yaml } #Store
-
-      if (page.body =~ /This facility has no availability for the date range you selected. Please choose from one of the following facilities which do have availability,/)
-        $logger.info "lafayette full, selecting orinda station"
-        page = page.form_with(:action => "/bart/reservations/date/") do |form|
-          button = form.radiobutton_with(:id => "type_id_37")
-          if button.nil?
-            $logger.error "orinda is also full"
-            return
-          else
-            button.check
-          end
-        end.submit
+      date = YAML.load_file "date.yml"
+      while date.saturday? || date.sunday?
+        date = date.succ
       end
 
-      $logger.info "(5/6) reserving"
-      page = page.form_with(:action => "/bart/reservations/reserve/").submit
-
-      $logger.info "(6/6) agreeing to terms and conditions"
-      page = page.link_with(:href => /confirm/).click
-      page = page.form_with(:id => "complete_form") do |form|
-        form.checkbox_with(:name => "conditions").check
+      $logger.info "(1/6) logging in"
+      page = @agent.get "https://select-a-spot.com/bart/"
+      credentials = YAML.load_file CREDENTIALS_PATH
+      page = page.form_with(:id => "site-login") do |login|
+        login.username = credentials[:username]
+        login.password = credentials[:password]
       end.submit
 
-      $logger.info "getting permit"
-      reservation = get_reservation @agent, date
-      download_permit @agent, reservation['Permit #:']
+      $logger.info "(2/6) selecting daily reservation"
+      page = page.link_with(:href => "/bart/reservations/facilities/?type=daily").click
+
+      $logger.info "(3/6) selecting lafayette station"
+      page = page.form_with(:action => "/bart/reservations/date/") do |form|
+        form.radiobutton_with(:id => "type_id_40").check
+      end.submit
+
+      $logger.info "(4/6) attempting to reserve #{date}"
+      # http://mechanize.rubyforge.org/Mechanize/Page.html
+      page = page.form_with(:action => "/bart/reservations/date/") do |form|
+        form.start_date =
+          form.end_date = date.to_s
+      end.submit
+
+      if (page.body =~ /Reservations cannot be made more than two months in advance/)
+        $logger.warn "too far in advance"
+        return
+      else
+        File.open('date.yml', 'w') {|f| f.write date.succ.to_yaml } #Store
+
+        if (page.body =~ /This facility has no availability for the date range you selected. Please choose from one of the following facilities which do have availability,/)
+          $logger.info "lafayette full, selecting orinda station"
+          page = page.form_with(:action => "/bart/reservations/date/") do |form|
+            button = form.radiobutton_with(:id => "type_id_37")
+            if button.nil?
+              $logger.warn "orinda is also full"
+              return
+            else
+              button.check
+            end
+          end.submit
+        end
+
+        $logger.info "(5/6) reserving"
+        page = page.form_with(:action => "/bart/reservations/reserve/").submit
+
+        $logger.info "(6/6) agreeing to terms and conditions"
+        page = page.link_with(:href => /confirm/).click
+        page = page.form_with(:id => "complete_form") do |form|
+          form.checkbox_with(:name => "conditions").check
+        end.submit
+
+        $logger.info "getting permit"
+        reservation = get_reservation @agent, date
+        download_permit @agent, reservation['Permit #:']
+      end
+    rescue Exception => e
+      $logger.error e.message
+      $logger.error e.backtrace.inspect
     end
   end
 
